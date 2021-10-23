812669b4c2704fc7b347c0ff453c98ff8cacad0a
x-Sumit Kharche <x-sumit.kharche@coyote.com>
2021-10-23 21:11:30 +0530

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a1e785688ad63271645253445681d0b2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '779a737a-eacd-48a8-8d05-5c4ab3a84141'
+};
